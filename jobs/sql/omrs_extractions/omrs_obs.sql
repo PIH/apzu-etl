b85bc1832e764ed19659d4e9218180b18ec842de
Michael Seaton <mseaton@pih.org>
2026-04-19 16:10:27 -0400

    Optimize obs and obs_group queries

--- jobs/sql/omrs_extractions/omrs_obs.sql
+++ jobs/sql/omrs_extractions/omrs_obs.sql
@@ -1,3 +1,26 @@
+drop temporary table if exists temp_concept_question;
+create temporary table temp_concept_question
+(
+    concept_id int,
+    name varchar(255)
+);
+insert into temp_concept_question (concept_id) select concept_id from concept;
+update temp_concept_question n
+set n.name = (
+    select cn.name from concept_name cn
+    where cn.concept_id = n.concept_id AND cn.voided = 0
+    order by if(cn.concept_name_type='FULLY_SPECIFIED', 0, 1), if(cn.locale='en', 0, 1), cn.locale_preferred desc limit 1
+);
+alter table temp_concept_question add index temp_concept_question_concept_idx (concept_id);
+
+drop temporary table if exists temp_concept_answer;
+create temporary table temp_concept_answer as select * from temp_concept_question;
+alter table temp_concept_answer add index temp_concept_answer_concept_idx (concept_id);
+
+drop temporary table if exists temp_obs_group;
+create temporary table temp_obs_group as select obs_group_id from obs where obs_group_id is not null and voided = 0;
+alter table temp_obs_group add index temp_obs_group_idx (obs_group_id);
+
 SELECT      o.obs_id,
             o.uuid,
             p.person_id as patient_id,
@@ -9,12 +32,8 @@ SELECT      o.obs_id,
             et.name as encounter_type,
             f.name as form,
             l.name as location,
-            (SELECT cn.name FROM concept_name cn
-             WHERE cn.concept_id = o.concept_id AND cn.voided = 0
-             ORDER BY IF(cn.concept_name_type='FULLY_SPECIFIED',0,1), IF(cn.locale='en',0,1), cn.locale_preferred DESC LIMIT 1) as concept,
-            (SELECT cn.name FROM concept_name cn
-             WHERE cn.concept_id = o.value_coded AND cn.voided = 0
-             ORDER BY IF(cn.concept_name_type='FULLY_SPECIFIED',0,1), IF(cn.locale='en',0,1), cn.locale_preferred DESC LIMIT 1) as value_coded,
+            q.name as concept,
+            a.name as value_coded,
             DATE(o.value_datetime) as value_date,
             o.value_numeric,
             o.value_text,
@@ -27,6 +46,10 @@ LEFT JOIN   encounter e ON o.encounter_id = e.encounter_id
 LEFT JOIN   encounter_type et ON et.encounter_type_id = e.encounter_type
 LEFT JOIN   form f ON f.form_id = e.form_id
 LEFT JOIN   location l ON l.location_id = e.location_id
+LEFT JOIN   temp_concept_question q on o.concept_id = q.concept_id
+LEFT JOIN   temp_concept_answer a on o.concept_id = a.concept_id
+LEFT JOIN   temp_obs_group g on o.obs_id = g.obs_group_id
 WHERE       o.voided = 0
 AND         p.voided = 0
-AND         o.obs_id NOT IN (SELECT DISTINCT obs_group_id FROM obs WHERE obs_group_id IS NOT NULL)
+AND         g.obs_group_id is null
+;
--- jobs/sql/omrs_extractions/omrs_obs_group.sql
+++ jobs/sql/omrs_extractions/omrs_obs_group.sql
@@ -1,3 +1,22 @@
+drop temporary table if exists temp_concept_name;
+create temporary table temp_concept_name
+(
+    concept_id int,
+    name varchar(255)
+);
+alter table temp_concept_name add index temp_concept_name_concept_idx (concept_id);
+insert into temp_concept_name (concept_id) select concept_id from concept;
+update temp_concept_name n
+set n.name = (
+    select cn.name from concept_name cn
+    where cn.concept_id = n.concept_id AND cn.voided = 0
+    order by if(cn.concept_name_type='FULLY_SPECIFIED', 0, 1), if(cn.locale='en', 0, 1), cn.locale_preferred desc limit 1
+);
+
+drop temporary table if exists temp_obs_group;
+create temporary table temp_obs_group as select obs_group_id from obs where obs_group_id is not null and voided = 0;
+alter table temp_obs_group add index temp_obs_group_idx (obs_group_id);
+
 SELECT      o.obs_id as obs_group_id,
             o.uuid,
             p.person_id as patient_id,
@@ -6,15 +25,15 @@ SELECT      o.obs_id as obs_group_id,
             TIME(o.obs_datetime) as obs_group_time,
             et.name as encounter_type,
             l.name as location,
-            (SELECT cn.name FROM concept_name cn
-             WHERE cn.concept_id = o.concept_id AND cn.voided = 0
-             ORDER BY IF(cn.concept_name_type='FULLY_SPECIFIED',0,1), IF(cn.locale='en',0,1), cn.locale_preferred DESC LIMIT 1) as concept,
+            n.name as concept,
             DATE(o.date_created) as date_created
 FROM        obs o
+INNER JOIN  temp_obs_group g on o.obs_id = g.obs_group_id
 INNER JOIN  person p ON o.person_id = p.person_id
 LEFT JOIN   encounter e ON o.encounter_id = e.encounter_id
 LEFT JOIN   encounter_type et ON et.encounter_type_id = e.encounter_type
 LEFT JOIN   location l ON l.location_id = e.location_id
+LEFT JOIN   temp_concept_name n on o.concept_id = n.concept_id
 WHERE       o.voided = 0
 AND         p.voided = 0
-AND         o.obs_id IN (SELECT DISTINCT obs_group_id FROM obs WHERE obs_group_id IS NOT NULL)
+;
