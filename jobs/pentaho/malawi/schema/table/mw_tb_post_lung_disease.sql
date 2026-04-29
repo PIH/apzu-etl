810e879c486dafe6fb268bb730a27d9ccb43f091
Michael Seaton <mseaton@pih.org>
2026-04-29 07:42:01 -0400

    Remove pentaho jobs again

--- jobs/pentaho/malawi/schema/table/mw_tb_post_lung_disease.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE mw_tb_post_lung_disease (
-  tb_post_lung_disease_visit_id 		INT NOT NULL AUTO_INCREMENT,
-  patient_id    				INT NOT NULL,
-  visit_date            		DATE,
-  location              		VARCHAR(255),
-  number_of_weeks 					INT,
-  second_visit_date 		DATE,
-     PRIMARY KEY (tb_post_lung_disease_visit_id)
-);
